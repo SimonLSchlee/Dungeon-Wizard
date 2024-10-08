994b096af9b216719041801f42a219731907d6ca
Nuno Das Neves <ndasneves@gmail.com>
2024-10-08 12:13:10 +1100

    test room gobbows, disable show hitboxes

--- src/debug.zig
+++ src/debug.zig
@@ -9,5 +9,5 @@ pub const show_thing_paths = false;
 pub const show_thing_coords_searched = false;
 pub const show_thing_collisions = false;
 pub const show_tilemap_grid = false;
-pub const show_hitboxes = true;
+pub const show_hitboxes = false;
 pub const show_selectable = false;
